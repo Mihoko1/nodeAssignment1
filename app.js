fa20ef5fc3c1b1f66cd68718f3a80d14d6377999
Mihoko1 <50882674+Mihoko1@users.noreply.github.com>
2019-09-26 23:54:38 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+/**
+* Module dependencies.
+*/
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , bodyParser=require("body-parser")
+,busboy = require("then-busboy")
+	,fileUpload = require('express-fileupload')
+  , path = require('path');
+
+  
+const fs = require("fs");
+//      multer: Node.js middleware for handling multipart/form-data.
+
+//var methodOverride = require('method-override');
+var session = require('express-session');
+var app = express();
+var mysql = require('mysql');
+var bodyParser=require("body-parser");
+
+var connection = mysql.createConnection({
+    user: 'root',
+    password: 'root',
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+    database: 'users',
+    multipleStatements: true
+});
+
+
+ 
+connection.connect();
+ 
+global.db = connection;
+
+
+ 
+// all environments
+app.set('port', process.env.PORT || 5000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge: 60000 }
+    }));
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/'
+}));
+ 
+// development only
+ 
+app.get('/', routes.index);//call for main index page
+app.get('/signup', user.signup);//call for signup page
+app.post('/signup', user.signup);//call for signup post 
+app.get('/login', routes.index);//call for login page
+app.post('/login', user.login);//call for login post
+app.get('/feed', user.feed);//call for feed post
+app.post('/feed', user.feed);//call for feed post
+app.get('/dashboard', user.dashboard);//call for dashboard page after login
+app.get('/logout', user.logout);//call for logout
+app.get('/profile',user.profile);//to render users profile
+app.post('/dashboard',user.dashboard);//call for profile picture update 
+//Middleware
+app.listen(5000);
---
+++ routes/user.js
@@ -0,0 +1,245 @@
+
+//---------------------------------------------signup page call------------------------------------------------------
+exports.signup = function(req, res){
+   message = '';
+   if(req.method == "POST"){
+      var post  = req.body;
+      var name= post.user_name;
+      var pass= post.password;
+      var fname= post.first_name;
+      var lname= post.last_name;
+      var mob= post.mob_no;
+       
+       var sql  ="SELECT * FROM users where user_name = '" + name +"'";
+       
+//       var check ="SELECT * FROM users where (user_name = '" + name +"' and password = '"+pass+"') "
+       
+       
+       
+       var query = db.query(sql, function(err, results){  
+          
+          if(err){
+              console.log(results);
+              console.log(results.length);
+              console.log(results[0]);
+          }
+           
+           
+        if(results.length){
+           
+           
+
+
+           message = "This user name is registered already. Please login.";
+            res.render('signup.ejs',{message: message});
+
+        }else{
+            
+            if(name == "" || pass == "" || fname == "" || mob == "" ){
+                message = "Missing information. Plase fill in all information.";
+                res.render('signup.ejs',{message: message});
+                
+            }else{
+       
+                var defaultPic= '../img/default-profile.jpg';
+               var sql = "INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`,`prof_pic`,`prof_pic_path`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass +  "', '" + defaultPic +"','" + defaultPic + "')";
+
+              var query = db.query(sql, function(err, result) {
+
+                 message = "Succesfully! Your account has been created.";
+                 res.render('signup.ejs',{message: message});
+              });
+            }
+
+        }
+           });
+
+      
+   } else {
+      res.render('signup');
+   }
+};
+ 
+//-----------------------------------------------login page call------------------------------------------------------
+exports.login = function(req, res){
+   var message = '';
+   var sess = req.session; 
+
+   if(req.method == "POST"){
+      var post  = req.body;
+      var name= post.user_name;
+      var pass= post.password;
+     
+      var sql="SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name`='"+name+"' and password = '"+pass+"'";                           
+      db.query(sql, function(err, results){  
+          
+          if(err){
+
+                console.log(results);
+              console.log(results.length);
+              console.log(results[0]);
+          }
+          
+         if(results.length){
+             
+            req.session.userId = results[0].id;
+            req.session.user = results[0];
+            console.log(results[0].id);
+            res.redirect('/feed');
+         }
+         else{
+            
+            message = 'Please imput correct information.';
+            res.render('index.ejs',{message: message});
+         }
+                 
+      });
+   } else {
+      res.render('index.ejs',{message: message});
+   }
+           
+};
+
+
+//-----------------------------------------------feed page call------------------------------------------------------
+exports.feed = function(req, res){
+   
+    var message = "";
+     var userId = req.session.userId;
+    
+     
+      
+    
+       if(userId == null){
+          res.redirect("/login");
+          return;
+       
+       }
+   
+      var sql="SELECT c.feed_id, c.feed_img, c.feed_text, c.feed_date, a.first_name, a.last_name, a.prof_pic_path FROM feed AS c, users AS a WHERE c.user_id = a.id";  
+    
+    var userInfo="SELECT * FROM users WHERE id = " + userId;
+       
+      db.query(sql + ";"+ userInfo,function(err, results){  
+          
+          if(err){
+              
+              console.log(results);
+              console.log(results.length);
+             
+              message = "Error. Please Login again.";
+              
+          }
+          
+          return res.render('feed.ejs', {feedData:results[0],data: results[1], message: message});    
+       });      
+};
+
+
+
+
+//-----------------------------------------------dashboard page functionality----------------------------------------------
+
+exports.dashboard = function(req, res, next){
+           
+    var userId = req.session.userId;
+       if(userId == null){
+          res.redirect("/login");
+          return;
+       
+       }
+
+
+    if(req.method == "POST"){
+    
+    
+	  if (!req.files || Object.keys(req.files).length === 0) 
+          
+        return res.status(400).send('No files were uploaded.');
+    
+ 
+		var file = req.files.avatar;
+		var img_name=file.name;
+ 
+        console.log(img_name);
+        
+        if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png"||file.mimetype == "image/gif" ){
+                                 
+         
+            var imgPath ='/img/upload_images/'+img_name;
+            file.mv(imgPath, function(err) {
+                             
+	           if (err)
+                   console.log("errorA:" + err);
+                   return res.status(500).send(err);
+                  
+                
+                var sql = "UPDATE users SET prof_pic_path ='" + imgPath + "' WHERE id =" + userId;
+                
+
+                db.query(sql +";" + function(err, results) {
+                    
+                    var sql2="SELECT * FROM `users` WHERE `id`='"+userId+"'";
+                    
+                    db.query(sql2, function(err, results) {
+                    
+                        return res.render('dashboard.ejs',{data: results});
+                    });
+                        
+                });
+              
+	       });
+        } else {
+            message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+            return res.render('dashboard.ejs',{message: message});
+        }
+      };  
+    
+    if(req.method == "GET"){
+    
+       var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";
+
+       db.query(sql, function(err, results){
+          return res.render('dashboard.ejs', {data:results});    
+       }); 
+    };
+
+};
+    
+    
+//------------------------------------logout functionality----------------------------------------------
+exports.logout=function(req,res){
+   req.session.destroy(function(err) {
+      res.redirect("/login");
+   })
+};
+//--------------------------------render user details after login--------------------------------
+exports.profile = function(req, res){
+
+    var message = '';
+   var userId = req.session.userId;
+       if(userId == null){
+          res.redirect("/login");
+          return;
+       
+       }
+        var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";          
+        db.query(sql, function(err, result){  
+            res.render('profile.ejs',{data:result});
+       });
+           
+};
+  
+//---------------------------------edit users details after login----------------------------------
+exports.editprofile=function(req,res){
+   var userId = req.session.userId;
+   if(userId == null){
+      res.redirect("/login");
+      return;
+   }
+
+   var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";
+   db.query(sql, function(err, results){
+      res.render('edit_profile.ejs',{data:results});
+   });
+};
